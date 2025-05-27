b3dfaa49e4a733cc32c71ca62dea8a7882229e1b
Bagzhan <bagzhankarl@gmail.com>
2025-05-28 00:54:40 +0500

    Обновлен скрипт деплоя: добавлена команда для остановки контейнеров перед обновлением и команда для их перезапуска с пересборкой. Исправлено имя переменной окружения в функции check_env на JWT_SECRET_KEY.

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 async def check_env():
     # Получаем все переменные окружения
     env_vars = {
-        "TEST_VAR": os.getenv("JWT_SECRET_KEY", "Not set"),
+        "JWT_SECRET_KEY": os.getenv("JWT_SECRET_KEY", "Not set"),
         "DB_HOST": os.getenv("DB_HOST", "Not set"),
         "DB_PORT": os.getenv("DB_PORT", "Not set"),
         "DB_NAME": os.getenv("DB_NAME", "Not set"),
