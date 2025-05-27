d3317691bf8d943bbb01c66b023c21c41a8e4253
Bagzhan <bagzhankarl@gmail.com>
2025-05-28 00:39:48 +0500

    Добавлен сервис backend в docker-compose, удалены файлы конфигурации .idea

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+@app.get("/check-env")
+async def check_env():
+    # Получаем все переменные окружения
+    env_vars = {
+        "TEST_VAR": os.getenv("TEST_VAR", "Not set"),
+        "DB_HOST": os.getenv("DB_HOST", "Not set"),
+        "DB_PORT": os.getenv("DB_PORT", "Not set"),
+        "DB_NAME": os.getenv("DB_NAME", "Not set"),
+        "DB_USER": os.getenv("DB_USER", "Not set"),
+    }
+    return env_vars
