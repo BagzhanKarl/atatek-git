601fd6225e13f057fc9ebf04c5560a238a7e86f7
Bagzhan <bagzhankarl@gmail.com>
2025-05-28 01:11:24 +0500

    Обновлен основной файл приложения: изменены метаданные FastAPI, добавлены обработчики исключений, исправлены импорты и версия приложения.

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 from src.app.config.response import http_exception_handler
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONRespons
-from fastapi.exceptions import HTTPException
-
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.responses import JSONResponse
 
 app = FastAPI(
-    title="ATATEK - онлайн шежіре",
-    version="3.0.0",
-    description="Жаңа нұсқа жаңа фреймворкта FastAPI",
+    title="Jaai.kz Auth Service",
+    description="Сервис аутентификации для проекта Jaai.kz, предназначенный для безопасной работы с пользователями и управлением доступом.",
+    version="1.0.0",
 )
 
+# Обработка HTTP ошибок
 app.add_exception_handler(HTTPException, http_exception_handler)
 
 # Обработка ошибок валидации (например, pydantic)
@@ -20,8 +19,7 @@ async def validation_exception_handler(request, exc):
         status_code=422,
         content={
             "status": "error",
-            "version": "3.0.0",
+            "version": "1.0.0",
             "data": {"detail": exc.errors()}
         }
     )
-
