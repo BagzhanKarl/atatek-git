fb2433c83171eaf77be0895947103a3462575b79
Bagzhan <bagzhankarl@gmail.com>
2025-05-28 17:39:19 +0500

    Добавлен новый маршрут для работы с новостями страниц в файле init.py. Импортирован роутер page_news и включен в инициализацию приложения с соответствующим префиксом и тегом.

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -5,6 +5,7 @@ from src.app.family.views import router as family_router
 from src.app.tree.views import router as tree_router
 from src.app.ticket.views import router as ticket_router
 from src.app.pages.views import router as pages_router
+from src.app.page_news.views import router as page_news_router
 
 
 def init_app(app):
@@ -12,6 +13,7 @@ def init_app(app):
     app.include_router(address_router, prefix="/address", tags=["address"])
     app.include_router(tree_router, prefix="/tree", tags=["tree"])
     app.include_router(pages_router, prefix="/pages", tags=["pages"])
+    app.include_router(page_news_router, prefix="/page_news", tags=["page_news"])
     app.include_router(ticket_router, prefix="/ticket", tags=["ticket"])
     app.include_router(family_router, prefix="/family", tags=["family"])
     app.include_router(db_router, prefix="/db", tags=["db"])
